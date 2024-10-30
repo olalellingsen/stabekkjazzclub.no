a56113e8e7160f914d2cc1daace7281b57488727
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2024-10-30 09:36:40 +0100

    add storage

--- stabekk-jazz-club/src/firebase.ts
+++ stabekk-jazz-club/src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,7 +13,9 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const db = getFirestore(app);
 
 // Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app);
+export { db, storage, app };
 export const auth = getAuth(app);
